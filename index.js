0d8f606d8265d17298086f1f5c2b8f55f8dfd7c6
Nowrin Nazim <diya.nowrin08@gmail.com>
2021-10-30 16:19:26 +0600

    make it better

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 async function run() {
+
     try {
         await client.connect()
-
         const database = client.db("DisneyLand")
         const activitessCollection = database.collection("activites")
         const ordersCollection = database.collection("orders")
@@ -29,9 +29,7 @@ async function run() {
 
         //  Get API
         app.get('/activities', async (req, res) => {
-            // const cursor = activitessCollection.find({})
             const cursor = activitessCollection.find({}).sort({ _id: -1 });
-
             const activites = await cursor.toArray();
             res.send(activites)
         })
@@ -47,10 +45,7 @@ async function run() {
         // booking
         app.get('/booking/:id', async (req, res) => {
             const id = req.params.id;
-            // console.log(req.params.id);
             const query = { _id: ObjectId(id) }
-            // const result = await activitessCollection.findOne(query)
-            // res.send(result);
             const activity = await activitessCollection.findOne(query)
             res.send(activity)
         });
@@ -65,31 +60,35 @@ async function run() {
 
         // Add My Order
         app.get('/orders', async (req, res) => {
-
             const email = req.query.email;
             const query = { email: (email) }
             const orders = await ordersCollection.find(query)
             const result = await orders.toArray()
-            // const order = req.body;
-            // const result = await ordersCollection.insertOne(order)
-            // res.json(result)
             res.json(result)
         })
 
+        // Approve or Reject
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await ordersCollection.deleteOne(query)
+            res.json(result)
+        });
 
 
-
+        // Get All orders
+        app.get('/orders/admin', async (req, res) => {
+            const cursor = ordersCollection.find({})
+            const result = await cursor.toArray()
+            res.json(result);
+        });
 
 
         // Delete Api
         app.delete("/orders/:id", async (req, res) => {
-            // console.log(req.params.id);
-
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await ordersCollection.deleteOne(query)
-            // const result = await activitessCollection.deleteOne({ _id: ObjectId(req.params.id) })
-            // res.send(result);
             res.json(result)
         })
     }
